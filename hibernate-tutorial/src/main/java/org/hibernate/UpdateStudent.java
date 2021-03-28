b294a48d89b888109aa1765e1e574c2c1f00aaaa
Ghenadie Caraman <ghennac1998@gmail.com>
2021-03-29 00:24:38 +0300

    HibernateBeginningDemo

---
+++ hibernate-tutorial/src/main/java/org/hibernate/UpdateStudent.java
@@ -0,0 +1,52 @@
+package org.hibernate;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.entities.Student;
+
+
+public class UpdateStudent {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory sessionFactory = new Configuration()
+                                            .configure("hibernate.cfg.xml")
+                                            .addAnnotatedClass(Student.class)
+                                            .buildSessionFactory();
+        // create session
+        Session session = sessionFactory.getCurrentSession();
+
+        try{
+
+            session  = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+
+            // get the student with an primary key
+            Student newStudent = session.get(Student.class, 1);
+            System.out.println("student:" + newStudent);
+
+            newStudent.setFirstName("Scooby");
+
+            session.getTransaction().commit();
+
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+
+            session.createQuery("delete from Student where id=2")
+                    .executeUpdate();
+
+            session.getTransaction().commit();
+            System.out.println("Done.");
+
+
+
+
+        }finally {
+            sessionFactory.close();
+        }
+
+
+    }
+
+
+}
